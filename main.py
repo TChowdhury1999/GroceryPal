a58b1f5735a916ee150be2d029be70ebe4dd8454
Tanjim Chowdhury <tanjim@live.co.uk>
2024-03-03 18:13:11 +0000

    added food data read in

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import polars as pl
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
@@ -6,7 +7,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.metrics import dp
-
+from kivy.core.window import Window
 from kivy.config import Config
 
 
@@ -23,22 +24,26 @@ class MainScreenLower(RelativeLayout):
     def update_confirm_button_position(self):
         confirm_button = self.ids.confirm_add_food_button
         toggle_button = self.ids.toggle_food_button
+        window_width, _ = Window.size
         if toggle_button.state == 'down':
+            confirm_button.pos_hint["right"] = 0.95
+            confirm_button.x = window_width * 0.95 - confirm_button.width
+            
+        else:
             confirm_button.pos_hint["right"] = 0
             confirm_button.x = 0
-        else:
-            confirm_button.pos_hint["right"] = 0.95
-            confirm_button.x = 300
-        print(confirm_button.x)
 
 
 class FoodCardsList(BoxLayout):
     def __init__(self, **kwargs):
         super(FoodCardsList, self).__init__(**kwargs)
-
-        # Create labels dynamically
-        for i in range(1, 14):  # Create 13 buttons as an example
-            label_text = f'Food {i}'
+        # load in food data
+        app_instance = App.get_running_app()
+        food_df = app_instance.load_food_data()
+        
+        # create a card for each stored food item
+        for row in food_df.rows(named=True):
+            label_text = row["name"]
             label = Button(text=label_text, size_hint=(1, None), height=dp(100))
             self.add_widget(label)
         
@@ -49,14 +54,24 @@ class FoodCardsList(BoxLayout):
 
 class GroceryPalApp(App):
     def build(self):
+        # set window size
+        Window.size = (400, 600)
         # Create the screen manager
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main_screen'))
-
         return sm
-    pass
+    
+    def load_food_data(self):
+        # load in food data
+        try:
+            data = pl.read_csv("data/food_data.csv")
+            return data
+        except Exception as e:
+            print(f"Error loading data: {e}")
+            return None
+        
+
+    
 
 if __name__ == '__main__':
-    Config.set('graphics', 'width', '400')
-    Config.set('graphics', 'height', '600')
     GroceryPalApp().run()
